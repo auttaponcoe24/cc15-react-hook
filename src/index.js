540cdbc28562b0e7ae9ca7f1eb6604e4284dbc7b
auttpon <auttaponcoe24@gmail.com>
2023-09-07 09:59:20 +0700

    update:useEffect

--- src/index.js
+++ src/index.js
@@ -2,23 +2,54 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+// s1 :
+
 function App() {
 	const [category, setCategory] = useState("");
 	const [count, setCount] = useState(0);
 
-	// every render, rerender
+	// Syntax : useEffect(setUp, dependenciesArray)
+	// ################################################
+	// ### 1 After render, rerender
 	// useEffect(() => {
 	// 	console.log("effect hook #1");
 	// });
 
-	// Syntax : useEffect(setUp, dependenciesArray)
-	useEffect(() => {
-		console.log("effect hook #2 - Category");
-	}, [category]);
+	// solu fetchUserProfile
+	// ### 2 After first render
+	// useEffect(() => {
+	//   console.log('Effect run')
+	// }, [])
+
+	// ### 3 After first render and After change state count(dependenciesArray change)
+	// useEffect(() => {
+	// 	console.log("effect hook #3 - Count");
+	// }, [count]);
+
+	// ################################################
 
 	useEffect(() => {
-		console.log("effect hook #3 - Count");
-	}, [count]);
+		// console.log("effect hook #2 - Category");
+
+		// url : https://jsonplaceholder.typicode.com/<categories>
+
+		// DEFINE FN
+		async function fetchLists() {
+			const BASE_URL = "https://jsonplaceholder.typicode.com";
+			try {
+				let res = await fetch(`${BASE_URL}/${category}`, {
+					method: "GET",
+				});
+				let data = await res.json();
+				console.log(data);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+
+		// CALL FN
+		if (category) fetchLists();
+	}, [category]);
 
 	console.log("render, rerender");
 	return (
